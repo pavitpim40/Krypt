fc2505f563be1992f1027e81bad1e99ee1e45242
pavitpim40 <pavitpim@gmail.com>
2022-03-03 23:37:09 +0700

    use Current Account for condition render logIn button

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -19,8 +19,8 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const { connectWallet } = useContext(TransactionContext);
-  console.log(connectWallet);
+  const { connectWallet,currentAccount } = useContext(TransactionContext);
+  
 
   const handleSumbit = () => {};
   return (
@@ -34,13 +34,13 @@ const Welcome = () => {
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             Krypto.
           </p>
-          <button
+        { !currentAccount &&  <button
             type="button"
             onClick={connectWallet}
             className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
           >
             <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          </button>}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
