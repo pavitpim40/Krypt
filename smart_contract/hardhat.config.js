603ffc39b71c0d9cff68b1fcfe1fd957f6935b9c
pavitpim40 <pavitpim@gmail.com>
2022-03-03 23:36:42 +0700

    fixed error and address key

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: "0.8.0",
-  networks: {
-    ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/I9F5Kw7r3sPLIX5s__GH2mKeUoYU8561",
-      accounts: [""],
-    },
-  },
+	solidity: "0.8.0",
+	networks: {
+		ropsten: {
+			url: "https://eth-ropsten.alchemyapi.io/v2/I9F5Kw7r3sPLIX5s__GH2mKeUoYU8561",
+			accounts: [
+				"c70d1f205e324cad7e175869e1ac5bbd576a673452fe01caeb3ef5f8c95604ec",
+			],
+		},
+	},
 };
